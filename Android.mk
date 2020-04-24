26d198987e1c360f2e2fdccf0e430359f8e1d90d
wishawa <wisha.wa@yandex.com>
2020-04-24 17:53:38 +0700

    remove extraneous endif

--- Android.mk
+++ Android.mk
@@ -21,4 +21,3 @@ ifeq ($(TARGET_DEVICE),X00D)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib/egl && pushd $(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 $(shell mkdir -p $(PRODUCT_OUT)/system/vendor/lib64/egl && pushd $(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 endif
-endif
